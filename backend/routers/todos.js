c1f1a391241a83b91d8ac4f7d2b4301025adad45
chicodevbr <chicodevbr@gmail.com>
2021-10-03 19:58:07 -0300

    try catch CRUD adjustment

--- backend/routers/todos.js
+++ backend/routers/todos.js
@@ -57,14 +57,13 @@ router.put('/:id', async (req, res) => {
   const { error } = schema.validate(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
-
-  const todo = await Todo.findById(req.params.id);
-
-  if (!todo) return res.status(404).send('Todo not found...');
-
-  const { name, author, isComplete, date, uid } = req.body;
-
   try {
+    const todo = await Todo.findById(req.params.id);
+
+    if (!todo) return res.status(404).send('Todo not found...');
+
+    const { name, author, isComplete, date, uid } = req.body;
+
     const updateTodo = await Todo.findByIdAndUpdate(
       req.params.id,
       {
@@ -84,11 +83,11 @@ router.put('/:id', async (req, res) => {
 });
 
 router.patch('/:id', async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-
-  if (!todo) return res.status(404).send('Todo not found...');
-
   try {
+    const todo = await Todo.findById(req.params.id);
+
+    if (!todo) return res.status(404).send('Todo not found...');
+
     const updateTodo = await Todo.findByIdAndUpdate(req.params.id, {
       isComplete: !todo.isComplete,
     });
@@ -101,11 +100,11 @@ router.patch('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-
-  if (!todo) return res.status(404).send('Todo not found...');
-
   try {
+    const todo = await Todo.findById(req.params.id);
+
+    if (!todo) return res.status(404).send('Todo not found...');
+
     const deleteTodo = await Todo.findByIdAndDelete(req.params.id);
 
     res.send(deleteTodo);
