1054a4409a0b0403b0668d7a164b3b34a27e166d
chicodevbr <chicodevbr@gmail.com>
2021-10-06 10:13:39 -0300

    style navbar

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,10 +1,79 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { Link, useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+
+  linkStyle: {
+    color: '#fafafa',
+    textDecoration: 'none',
+  },
+}));
+
 const NavBar = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleSignOut = () => {
+    //signout the user
+    history.push('/signin');
+  };
+
+  const handleSignUp = () => {
+    //signout the user
+    history.push('/signup');
+  };
   return (
-    <>
-      <h2>NavBar</h2>
-    </>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/" className={classes.linkStyle}>
+              ToDo App
+            </Link>
+          </Typography>
+          <Typography>Signed in as George</Typography>
+          <Button color="inherit" onClick={() => handleSignOut()}>
+            SignOut
+          </Button>
+          <Button color="inherit">
+            <Link className={classes.linkStyle} to="/signin">
+              SignIn
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link className={classes.linkStyle} to="/signup">
+              SignUp
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
